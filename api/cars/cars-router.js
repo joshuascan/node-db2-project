53a93831e800fe8e348bfe515c0997f9f5b1f1fa
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-13 13:05:11 -0700

    create scaffolding for cars-router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,17 @@
-// DO YOUR MAGIC
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.json("get all cars");
+});
+
+router.get("/:id", (req, res, next) => {
+  res.json("get car by id");
+});
+
+router.post("/", (req, res, next) => {
+  res.json("create new car");
+});
+
+module.exports = router;
