4fe8ea93b8892d200dab8c1637dd718f309f3c93
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-13 12:44:47 -0700

    create cars seed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+exports.seed = function (knex) {
+  return knex("cars")
+    .truncate()
+    .then(function () {
+      return knex("cars").insert([
+        {
+          vin: 18472018,
+          make: "Volkswagen",
+          model: "Golf",
+          mileage: 24975,
+          title: "clean",
+          transmission: "automatic",
+        },
+        {
+          vin: 83650173,
+          make: "BMW",
+          model: "325",
+          mileage: 12473,
+          transmission: "manual",
+        },
+        {
+          vin: 17693753,
+          make: "Honda",
+          model: "Civic",
+          mileage: 34998,
+          title: "salvage",
+        },
+        { vin: 65837401, make: "Kia", model: "Soul", mileage: 42653 },
+      ]);
+    });
+};
