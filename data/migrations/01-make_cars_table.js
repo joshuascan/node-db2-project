b6cdb6c498b352a96bdc634140f26feca88cfaa4
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-13 12:38:20 -0700

    create cars schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable("cars", (table) => {
+    table.increments("car_id");
+    table.text("vin").unique().notNullable();
+    table.text("make").notNullable();
+    table.text("model").notNullable();
+    table.decimal("mileage").notNullable();
+    table.text("title");
+    table.text("transmission");
+  });
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars");
 };
