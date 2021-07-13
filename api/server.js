bd25e39aa57339c7982823cbd68cc5db174943b1
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-07-13 12:48:47 -0700

    completes server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
-const express = require("express")
+const express = require("express");
+const server = express();
 
-const server = express()
+const carsRouter = require("./cars/cars-router");
 
-// DO YOUR MAGIC
+server.use(express.json());
 
-module.exports = server
+server.use("/api/cars", carsRouter);
+
+module.exports = server;
